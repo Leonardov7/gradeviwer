070e71fd6ec36e15ace81382cb7d42d2b6573f07
leonardo <leonardov7@gmail.com>
2022-04-03 17:57:32 -0500

    First Commmit

---
+++ android/app/src/main/java/co/edu/ucundinamarca/gradeviwer/MainActivity.java
@@ -0,0 +1,6 @@
+package co.edu.ucundinamarca.gradeviwer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeStart createState() => HomeStart();
+}
+
+class HomeStart extends State<Home> {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bienvenidos App Línea 2',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Visor de calificaciones'),
+
+        ),
+        body:(
+
+        ),
+      ),
+    );
+  }
+}
